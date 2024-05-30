c3c69e0f24b9745fdf09c16bd5d42d8243cea853
basu101 <kartavyasrivastav1947@gmail.com>
2024-05-30 13:23:21 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,3 +1,15 @@
+variable "access_key" {
+    type = string
+    default = ""
+
+}
+
+variable "secret_key" {
+    type = string
+    default = ""
+
+}
+
 variable "region" {
     type = string
     default = ""
